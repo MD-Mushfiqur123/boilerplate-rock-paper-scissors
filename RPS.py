3ce189c47fc816e2c79a9fbfa80152d65009d4f2
Md Mushfiqur Rahim <20mahin2020@gmail.com>
2026-08-31 05:15:55 +0000

    The Polymath Hack: RPS Completed!

--- RPS.py
+++ RPS.py
@@ -1,10 +1,31 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
-
-def player(prev_play, opponent_history=[]):
+def player(prev_play, opponent_history=[], play_order={}):
+    # গ্লিচ ফিক্স: শুরুতে মেমরি ক্লিয়ার করার বদলে 'R' দিয়ে গেম কন্টিনিউ রাখা
+    if not prev_play:
+        prev_play = "R"
+        
     opponent_history.append(prev_play)
-
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
-
-    return guess
+    prediction = "P"
+    
+    n = 5 # গ্লোবাল প্যাটার্ন ম্যাচিংয়ের সুইট-স্পট
+    
+    if len(opponent_history) >= n:
+        last_sequence = "".join(opponent_history[-n:])
+        # গ্লোবাল মেমরি ট্র্যাক করা হচ্ছে
+        play_order[last_sequence] = play_order.get(last_sequence, 0) + 1
+        
+        last_n_minus_1 = "".join(opponent_history[-(n-1):])
+        
+        potential_plays = [
+            last_n_minus_1 + "R",
+            last_n_minus_1 + "P",
+            last_n_minus_1 + "S",
+        ]
+        
+        sub_order = {k: play_order[k] for k in potential_plays if k in play_order}
+        
+        if sub_order:
+            prediction = max(sub_order, key=sub_order.get)[-1:]
+            
+    # পারফেক্ট কাউন্টার-অ্যাটাক
+    ideal_response = {'P': 'S', 'R': 'P', 'S': 'R'}
+    return ideal_response[prediction]
